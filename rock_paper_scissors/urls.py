2b63468b205a53bb67d7c45b7c1a750e87872a33
Giovanni Berti <dev.giovanniberti@gmail.com>
2023-01-02 20:38:34 +0100

    feat(views): add basic RPS view

--- rock_paper_scissors/urls.py
+++ rock_paper_scissors/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from rock_paper_scissors import views
+
+urlpatterns = [
+    path('', views.RockPaperScissorsView.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- rock_paper_scissors/views.py
+++ rock_paper_scissors/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import random
+from django.http import HttpResponse, JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+from rock_paper_scissors.models import Choice, Turn
+from rock_paper_scissors.serializers import ChoiceSerializer, TurnSerializer
+from loguru import logger
 
-# Create your views here.
+class RockPaperScissorsView(APIView):
+    def post(self, request):
+        data = JSONParser().parse(request)
+        choice_serializer = ChoiceSerializer(data=data)
+
+        if not choice_serializer.is_valid():
+            return Response({ "error": choice_serializer.error_messages }, status=status.HTTP_400_BAD_REQUEST)
+
+        player_choice = choice_serializer.data["choice"]
+        ai_choice = random.choice(list(Choice.InnerChoice))
+
+        turn_outcome = turn(player_choice=player_choice, ai_choice=ai_choice)
+        turn_serializer = TurnSerializer(turn_outcome)
+
+        logger.info("Player played {} while AI played {}. Player outcome={}", player_choice, ai_choice, turn_outcome.outcome)
+
+        return Response(turn_serializer.data)
+
+def turn(player_choice, ai_choice):
+    ROCK = Choice.InnerChoice.ROCK
+    PAPER = Choice.InnerChoice.PAPER
+    SCISSORS = Choice.InnerChoice.SCISSORS
+
+    if player_choice == ai_choice:
+        return Turn(outcome=Turn.Outcome.DRAW)
+    elif (player_choice, ai_choice) == (PAPER, ROCK) or \
+        (player_choice, ai_choice) == (ROCK, SCISSORS) or \
+        (player_choice, ai_choice) == (SCISSORS, PAPER):
+        return Turn(outcome=Turn.Outcome.WIN)
+
+    return Turn(outcome=Turn.Outcome.LOSE)
