cb6da972892f8bdd3ce795b78e297f53eb05f493
Giovanni Berti <dev.giovanniberti@gmail.com>
2023-01-02 20:37:55 +0100

    feat(models): add basic RPS models

--- rock_paper_scissors/models.py
+++ rock_paper_scissors/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Choice(models.Model):
+    class InnerChoice(models.TextChoices):
+        ROCK = "ROCK"
+        PAPER = "PAPER"
+        SCISSORS = "SCISSORS"
+
+    choice = models.CharField(choices=InnerChoice.choices, max_length=10, primary_key=True)
+
+class Turn(models.Model):
+    class Outcome(models.TextChoices):
+        WIN = "win"
+        LOSE = "lose"
+        DRAW = "draw"
+
+    outcome = models.CharField(choices=Outcome.choices, max_length=4, primary_key=True)
+
